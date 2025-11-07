ed7b6bb96861cbd72493416559048b061bd46b9a
GottaGoQuick <ayy12246@gmail.com>
2025-11-07 14:14:38 +0200

    Add files via upload

---
+++ no function/ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        // Import the mouse_event function from user32.dll
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        // Define constants for mouse events
+        private const uint MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const uint MOUSEEVENTF_LEFTUP = 0x04;
+
+        static void Main(string[] args)
+        {
+            int start = 0;
+            while (start > -1)
+            {
+                Console.WriteLine("Press p to start");
+                string pchecker = Console.ReadLine();
+                if (pchecker == "p")
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        // Simulate mouse click
+                        mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                        // Add a slight delay to avoid overwhelming the system
+                        System.Threading.Thread.Sleep(10);
+                    }
+                    start = -1;
+                }
+                else
+                {
+                    start++;
+                }
+            }
+        }
+    }
+}
---
+++ no function/primal numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace primal_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* במתמטיקה מספר נקרא ראשוני אם הוא מתחלק רק בעצמו ובאחד. לדוגמה: 2,3,5,7 הם מספרים ראשוניים ואילו 4 אינו ראשוני, כי 2 מחלק אותו. 
+כיתבו תכנית הקולטת המדפיסה את כל המספרים הראשוניים בין 1 ל 1000 
+            */
+            int counter2 = 0;
+            int counter = 0;
+            for (int num = 1; num <= 1000; num++)
+            {
+                counter = 0;
+                for (int j = 1; j <= num; j++)
+                {
+                    if (num%j == 0)
+                    {
+                        counter++;
+                    }
+                }
+                if (counter <= 2&&num!=1) 
+                {
+                    Console.WriteLine("{0} is a primal number", num);
+                    counter2++;
+                }
+                
+            }
+            Console.WriteLine("there is {0} primal numbers between 1-1000",counter2);
+
+
+        }
+    }
+}
