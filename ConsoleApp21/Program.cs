367dae94ab91142422af6fe57be25f31b3ce2732
GottaGoQuick <ayy12246@gmail.com>
2025-11-07 14:01:17 +0200

    Add files via upload

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics.Metrics;
+using System.Threading.Channels;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* קילטו שלישיות מספרים עד אשר שנקלטת שלישייה שסכומה גבוה מ 100
+יש להדפיס את סכום של כל השלישיות וכמה שלישיות נקלטו
+(אין להחשיב את הסכום הגדול מ 100 בחישוב)
+            */
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            int numfinal = 0;
+            int counter = 0;
+            while (num1 + num2 + num3 <= 100)
+            {
+                Console.WriteLine("give me a first number");
+                 num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("give me a second number");
+                 num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("give me a third number");
+                 num3 = int.Parse(Console.ReadLine());
+                numfinal = (num1 + num2 + num3); 
+                if (numfinal <= 100)
+                {
+                    Console.WriteLine("the sum is not greater then 100 try again");
+                    Console.WriteLine(" ");
+                    counter++;
+                }
+            }
+           
+            Console.WriteLine("Great Job!");
+            Console.WriteLine("your numbers together add up to more then 100 this is your numbers:");
+            Console.WriteLine("your first number is = {0}",num1);
+            Console.WriteLine("your second number is = {0}", num2);
+            Console.WriteLine("your third number is = {0}", num3);
+            Console.WriteLine("the amount of all of the numbers together is = {0}",numfinal);
+            Console.WriteLine("the count of all the three numbers your put in prier to getting a sum of 100 or above is = {0}",counter);
+            if (counter == 0)
+            {
+                Console.WriteLine("you got it on first attempt! maybe try again with more attempts to get diffrent information");
+            }
+        }
+    }
+}
---
+++ yes or no do you know/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace yes_or_no_do_you_know
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* כיתבו תוכנית אשר קולטת שלם ומדפיסה כן(באנגלית) אם ספרת האחדות שווה לספרה השמאלית , 
+             * אחרת תדפיס לא(באנגלית).
+             */
+            int digit1 = 0;
+            int digit2 = 0;
+            int checker = 0;
+            int coolchecker = 0;
+            while (checker==0)
+            {
+                coolchecker = 0;
+                Console.WriteLine("give me a three or above digit number:");
+                int num = int.Parse(Console.ReadLine());
+                if (num > 99)
+                {
+                    digit1 = num % 10;
+                    while (num > 0)
+                    {
+                        digit2 = num % 10;
+                        num = num / 10;
+                    }
+                    if(digit1 == digit2)
+                    {
+                        checker++;
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("the number doesnt have three digits or its a negative try again!");
+                    coolchecker++;
+                }
+                if (checker == 0&&coolchecker==0)
+                {
+                    Console.WriteLine("No");
+                    checker++;
+                }
+                else if (coolchecker==0)
+                {
+                    Console.WriteLine("Yes");
+                }
+            }
+            
+
+        }
+    }
+}
