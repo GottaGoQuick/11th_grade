fd4e009d73ed87e62e5595e57880f8cee3c4a277
gottagoquick <ayy12246@gmail.com>
2025-11-10 09:27:27 +0200

    a

--- no function/ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
+++ no function/ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1ee8b2dce00477655fdf46ee5673fe6600f64a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+991bbaeb913a2511ee59a0713d82928033b87707")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
